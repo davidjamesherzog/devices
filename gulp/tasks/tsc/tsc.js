27de96c00830f6a03b50ade442d17f9e6f0ff806
Dave Herzog <dherzog@dramafever.com>
2016-11-21 08:02:02 -0500

    Get rid of transpiling server code for now

--- gulp/tasks/tsc/tsc.js
+++ gulp/tasks/tsc/tsc.js
@@ -5,6 +5,7 @@ module.exports = function (gulp, config, plugins) {
    * Compiles *.js files, sourcemaps,
    */
   return {
-    deps: ['tsc:client', 'tsc:server']
+    deps: ['tsc:client']
+    //deps: ['tsc:client', 'tsc:server'] - todo - figure out why this transpiles to both client and server
   };
 };
